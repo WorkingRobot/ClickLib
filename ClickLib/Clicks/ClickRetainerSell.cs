b00ec919a5acfb420c270687b69a9abff3ec5d12
Asriel Camora <asriel@camora.dev>
2023-06-10 17:19:17 -0700

    Add retainer sales clicks

---
+++ ClickLib/Clicks/ClickRetainerSell.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using ClickLib.Attributes;
+using ClickLib.Bases;
+using FFXIVClientStructs.FFXIV.Client.UI;
+
+namespace ClickLib.Clicks;
+
+/// <summary>
+/// Addon RetainerSell.
+/// </summary>
+public sealed unsafe class ClickRetainerSell : ClickBase<ClickRetainerSell, AddonRetainerSell>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ClickRetainerSell"/> class.
+    /// </summary>
+    /// <param name="addon">Addon pointer.</param>
+    public ClickRetainerSell(IntPtr addon = default)
+        : base("RetainerSell", addon)
+    {
+    }
+
+    public static implicit operator ClickRetainerSell(IntPtr addon) => new(addon);
+
+    /// <summary>
+    /// Instantiate this click using the given addon.
+    /// </summary>
+    /// <param name="addon">Addon to reference.</param>
+    /// <returns>A click instance.</returns>
+    public static ClickRetainerSell Using(IntPtr addon) => new(addon);
+
+    /// <summary>
+    /// Click the confirm button.
+    /// </summary>
+    [ClickName("retainer_sale_confirm")]
+    public void Confirm()
+        => this.UnitBase->FireCallbackInt(0);
+
+    /// <summary>
+    /// Click the cancel button.
+    /// </summary>
+    [ClickName("retainer_sale_cancel")]
+    public void Cancel()
+        => this.ClickAddonButton(this.Addon->Cancel, 22);
+
+    /// <summary>
+    /// Click the compare prices button.
+    /// </summary>
+    [ClickName("retainer_sale_compare_prices")]
+    public void ComparePrices()
+        => this.ClickAddonButton(this.Addon->ComparePrices, 4);
+
+    /// <summary>
+    /// Click the commence button.
+    /// </summary>
+    /// <param name="quantity">Quantity to sell.</param>
+    public void SetQuantity(int quantity)
+        => this.Addon->Quantity->SetValue(quantity);
+
+    /// <summary>
+    /// Click the commence button.
+    /// </summary>
+    /// <param name="price">Price to sell at.</param>
+    public void SetAskingPrice(int price)
+        => this.Addon->AskingPrice->SetValue(price);
+}
---
+++ ClickLib/Clicks/ClickRetainerSellList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+using ClickLib.Attributes;
+using ClickLib.Bases;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace ClickLib.Clicks;
+
+/// <summary>
+/// Addon RetainerSellList.
+/// </summary>
+public sealed unsafe class ClickRetainerSellList : ClickBase<ClickRetainerSellList>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ClickRetainerSellList"/> class.
+    /// </summary>
+    /// <param name="addon">Addon pointer.</param>
+    public ClickRetainerSellList(IntPtr addon = default)
+        : base("RetainerSellList", addon)
+    {
+    }
+
+    /// <summary>
+    /// Gets the number of sale listings.
+    /// </summary>
+    public int ListingCount => ((AtkUnitBase*)this.AddonAddress)->GetComponentListById(11)->ListLength;
+
+    public static implicit operator ClickRetainerSellList(IntPtr addon) => new(addon);
+
+    /// <summary>
+    /// Instantiate this click using the given addon.
+    /// </summary>
+    /// <param name="addon">Addon to reference.</param>
+    /// <returns>A click instance.</returns>
+    public static ClickRetainerSellList Using(IntPtr addon) => new(addon);
+
+    /// <summary>
+    /// Click a listing.
+    /// </summary>
+    /// <param name="index">Listing index.</param>
+    public void Listing(int index)
+        => this.FireCallback(0, index, 1);
+
+    /// <summary>
+    /// Put an item from the inventory up for sale.
+    /// </summary>
+    /// <param name="inventoryId">Inventory index.
+    /// 6 => Equipment
+    /// 9 => Crystals, slot id is up-to-down left-to-right
+    /// 17 => Retainer crystals
+    /// 48 - 51 => Inventory1 - Inventory4
+    /// 52 - 56 => Retainer inventory 1-5
+    /// 57 - 68 => Armory chest
+    /// 69 - 70 => Chocobo saddlebag</param>
+    /// <param name="slot">Inventory slot index.</param>
+    public void SellItem(int inventoryId, int slot)
+        => this.FireCallback(2, inventoryId, slot);
+
+#pragma warning disable SA1134,SA1516,SA1600
+    [ClickName("select_listing1")] public void Listing1() => this.Listing(0);
+    [ClickName("select_listing2")] public void Listing2() => this.Listing(1);
+    [ClickName("select_listing3")] public void Listing3() => this.Listing(2);
+    [ClickName("select_listing4")] public void Listing4() => this.Listing(3);
+    [ClickName("select_listing5")] public void Listing5() => this.Listing(4);
+    [ClickName("select_listing6")] public void Listing6() => this.Listing(5);
+    [ClickName("select_listing7")] public void Listing7() => this.Listing(6);
+    [ClickName("select_listing8")] public void Listing8() => this.Listing(7);
+    [ClickName("select_listing9")] public void Listing9() => this.Listing(8);
+    [ClickName("select_listing10")] public void Listing10() => this.Listing(9);
+    [ClickName("select_listing11")] public void Listing11() => this.Listing(10);
+    [ClickName("select_listing12")] public void Listing12() => this.Listing(11);
+    [ClickName("select_listing13")] public void Listing13() => this.Listing(12);
+    [ClickName("select_listing14")] public void Listing14() => this.Listing(13);
+    [ClickName("select_listing15")] public void Listing15() => this.Listing(14);
+    [ClickName("select_listing16")] public void Listing16() => this.Listing(15);
+    [ClickName("select_listing17")] public void Listing17() => this.Listing(16);
+    [ClickName("select_listing18")] public void Listing18() => this.Listing(17);
+    [ClickName("select_listing19")] public void Listing19() => this.Listing(18);
+    [ClickName("select_listing20")] public void Listing20() => this.Listing(19);
+#pragma warning restore SA1134,SA1516,SA1600
+}
